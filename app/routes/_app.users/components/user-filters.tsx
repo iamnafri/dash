5e107bb5610da7b066c6b430cedb7714a1baecb2
Irfan <sikumbangirfann@gmail.com>
2023-12-24 15:47:44 +0800

    fix: prevent scroll when select roles and change page when filter changes

--- app/routes/_app.users/components/user-filters.tsx
+++ app/routes/_app.users/components/user-filters.tsx
@@ -1,7 +1,7 @@
 import { Select, SelectItem } from "@nextui-org/react";
 import { Role } from "@prisma/client";
 import { useSearchParams } from "@remix-run/react";
-import { useRef } from "react";
+import { KeyboardEvent, useRef } from "react";
 import { Icon } from "~/components/ui/icon";
 import { Input } from "~/components/ui/input";
 
@@ -15,6 +15,39 @@ export function UserFilters({ roles }: UserFiltersProps) {
   const searchParam = searchParams.get("search") || "";
   const rolesParam = searchParams.get("roles");
 
+  function onSelectRoles(roles: React.ChangeEvent<HTMLSelectElement>) {
+    setSearchParams(
+      (prev) => {
+        if (prev.has("page")) prev.set("page", "1");
+        prev.set("roles", roles.target.value);
+        return prev;
+      },
+      { preventScrollReset: true }
+    );
+  }
+
+  function onSearchInputKeydown(
+    event: KeyboardEvent<HTMLInputElement> | KeyboardEvent
+  ) {
+    const searchInputValue = searchInputRef.current?.value;
+    if (event.key === "Enter") {
+      setSearchParams((prev) => {
+        if (prev.has("page")) prev.set("page", "1");
+        prev.set("search", String(searchInputValue));
+        return prev;
+      });
+    }
+  }
+
+  function onSearchInputBlur() {
+    const searchInputValue = searchInputRef.current?.value;
+    setSearchParams((prev) => {
+      if (prev.has("page")) prev.set("page", "1");
+      prev.set("search", String(searchInputValue));
+      return prev;
+    });
+  }
+
   return (
     <div className="flex flex-col gap-unit-5 md:flex-row md:items-center">
       <Select
@@ -24,12 +57,7 @@ export function UserFilters({ roles }: UserFiltersProps) {
         size="sm"
         className="w-full sm:max-w-[30%]"
         defaultSelectedKeys={rolesParam ? rolesParam.split(",") : []}
-        onChange={(roles) => {
-          setSearchParams((prev) => {
-            prev.set("roles", roles.target.value);
-            return prev;
-          });
-        }}
+        onChange={onSelectRoles}
       >
         {roles.map((role) => (
           <SelectItem key={role.name} value={role.name} className="capitalize">
@@ -46,15 +74,8 @@ export function UserFilters({ roles }: UserFiltersProps) {
         startContent={<Icon name="search" size="sm" />}
         size="sm"
         className="w-full sm:max-w-[70%]"
-        onKeyDown={(e) => {
-          const searchInputValue = searchInputRef.current?.value;
-          if (e.key === "Enter") {
-            setSearchParams((prev) => {
-              prev.set("search", String(searchInputValue));
-              return prev;
-            });
-          }
-        }}
+        onKeyDown={onSearchInputKeydown}
+        onBlur={onSearchInputBlur}
       />
     </div>
   );
